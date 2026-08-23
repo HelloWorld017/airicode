02a6769d25d2bf728dd5473964dad6f513c33fa7
nenw <khi@nenw.dev>
2026-08-23 14:41:45 +0000

    feat: initialize flake

---
+++ AGENTS.md
@@ -0,0 +1,1 @@
+* Use `nix develop -c [command]` if it complains that cargo, ... does not exist. (Do not search for executables.)
---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "workspace for airicode";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+    systems.url = "github:nix-systems/default-linux";
+  };
+
+  outputs = { nixpkgs, crane, fenix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+      rustToolchain = fenix.packages.${system}.combine [
+        fenix.packages.${system}.stable.toolchain
+        fenix.packages.${system}.stable.rust-src
+      ];
+
+      craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+      libBuildInputs = with pkgs; [
+        libayatana-appindicator
+        libGL
+        libxkbcommon
+        wayland
+      ];
+      desktopBuildInputs = with pkgs; (libBuildInputs ++ [
+        electron
+      ]);
+      packageFor = cargoPackage:
+        craneLib.buildPackage {
+          pname = cargoPackage;
+          version = "0.0.0";
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+          cargoExtraArgs = "--package ${cargoPackage}";
+          meta.mainProgram = cargoPackage;
+        };
+    in {
+      packages = {
+        default = packageFor "airicode";
+      };
+
+      devShells.default = craneLib.devShell {
+        packages = [ pkgs.lld ] ++ desktopBuildInputs;
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libBuildInputs;
+      };
+    });
+}
